56d36dce10f6b200534ed8ea96cf5e5231957f74
KaishenLin <162913816+KaishenLin@users.noreply.github.com>
2025-02-10 19:07:52 +0100

    Delete 笔记.m

--- 笔记.m
+++
@@ -1,8 +0,0 @@
-%%% 接下来的几步：
-% 1. min- max space /initial 
-% 2. add space penalty
-% 3. AF-fft  
-% 4. comapre the results, get trade off of sparsity level and psll (X)
-% 5. comapre with KUNLEI's results
-%%%%！！！！save the data
-
